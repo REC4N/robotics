558bc2d808b29670b35129d20a53dafb2ba74fdc
recan <erecancojpajarez@gmail.com>
2021-09-19 23:20:18 -0600

    Updated navigation_v2 with meaningful comments

--- dev_ws/src/navigation/navigation/navigation_v2.py
+++ dev_ws/src/navigation/navigation/navigation_v2.py
@@ -4,40 +4,41 @@
 # Import libraries.
 import rclpy
 from rclpy.node import Node
-
 from geometry_msgs.msg import Twist
 
 
 class Navigation(Node):
     """
+    Navigation node for robotic system.
     """
 
     def __init__(self):
-        # Create node
+        # Create Navigation node.
         super().__init__('navigation')
+        # Define and initialize variables for motor control use.
         self.keyboard_vx = 0.0
         self.keyboard_vtheta = 0.0
-        # Create publisher node
+        # Create publisher node for target velocity of motor control.
         self.publisher_ = self.create_publisher(Twist, 'target_vel', 10)
         # Send messages every 10 Hz
         timer_period = 0.1  # seconds 
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        # Create subscriber node to a 'motor_feedback' topic and expects a Twist message.
+        self.timer = self.create_timer(timer_period, self.publish_motor_control)
+        # Create subscriber node to a 'platform_vel' topic and expects a Twist message.
         self.subscription_motor_control = self.create_subscription(
             Twist,
             'platform_vel',
-            self.listener_callback_motor_control,
+            self.motor_control_callback,
             10)
         self.subscription_motor_control  # prevent unused variable warning
         # Create subscriber node to a 'cmd_vel' topic and expects a Twist message.
         self.subscription_keyboard = self.create_subscription(
             Twist,
             'cmd_vel',  # Topic of teleop_twist_keyboard node.
-            self.listener_callback_keyboard,
+            self.keyboard_callback,
             10)
         self.subscription_keyboard  # prevent unused variable warning
 
-    def timer_callback(self):
+    def publish_motor_control(self):
         # Publish a twist message.
         msg = Twist()
         msg.linear.x = float(self.keyboard_vx)
@@ -45,11 +46,16 @@ class Navigation(Node):
         self.publisher_.publish(msg)
         self.get_logger().info('(Vx: "%2.2f", Vtheta: "%2.2f")' % (msg.linear.x, msg.angular.z))
 
-    def listener_callback_motor_control(self, msg):
-        # Print useful information to terminal.
+    def motor_control_callback(self, msg):
+        """
+        Recieves current platform velocity.
+        """
         self.get_logger().info('Arduino sent: Linear: "%.2f", Angular: "%.2f"' % (msg.linear.x, msg.angular.z))
 
-    def listener_callback_keyboard(self, msg):
+    def keyboard_callback(self, msg):
+        """
+        Assigns the recieved message from the keyboard into class variables.
+        """
         self.keyboard_vx = msg.linear.x
         self.keyboard_vtheta = msg.angular.z
 
